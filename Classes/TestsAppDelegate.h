05f8089415fb717f26ddd2f13e0d93f90d2e42bf
Jonathan <fontajo@yahoo.com>
2010-12-12 21:00:59 -0500

    Added MIT license, Added GHUnit and Test target for continuous integration support

---
+++ Classes/TestsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TestsAppDelegate.h
+//  Tests
+//
+//  Created by JONATHAN on 12/12/10.
+//  Copyright 2010 Personal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TestsAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Classes/TestsAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  TestsAppDelegate.m
+//  Tests
+//
+//  Created by JONATHAN on 12/12/10.
+//  Copyright 2010 Personal. All rights reserved.
+//
+
+#import "TestsAppDelegate.h"
+
+@implementation TestsAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+@end
