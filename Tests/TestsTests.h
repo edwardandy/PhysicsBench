8af16aad690545ceb1b2354c657f2b39903626e5
Jonathan <fontajo@yahoo.com>
2010-12-12 23:36:16 -0500

    Fixup script, hopefully

---
+++ Tests/TestsTests.h
@@ -0,0 +1,17 @@
+//
+//  TestsTests.h
+//  GraphicsCocos2d
+//
+//  Created by JONATHAN on 12/12/10.
+//  Copyright 2010 Personal. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TestsTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ Tests/TestsTests.m
@@ -0,0 +1,26 @@
+//
+//  TestsTests.m
+//  GraphicsCocos2d
+//
+//  Created by JONATHAN on 12/12/10.
+//  Copyright 2010 Personal. All rights reserved.
+//
+
+#import "TestsTests.h"
+
+
+@implementation TestsTests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+@end
